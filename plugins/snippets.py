fbe6c8f16483a218daa323a14b8ef758c9af55ce
Dan Elkouby <streetwalrus@codewalr.us>
2018-03-21 22:54:20 +0000

    Add snippets plugin

---
+++ plugins/snippets.py
@@ -0,0 +1,22 @@
+import logging
+import re
+
+import regex
+from telethon import events, utils
+
+from __main__ import client
+
+logger = logging.getLogger(__name__)
+
+snips = {
+        "shrug": "¯\_(ツ)_/¯",
+        "lenny": " ( ͡° ͜ʖ ͡°) ",
+        }
+
+@client.on(events.NewMessage(outgoing=True, pattern=re.compile(r"^!(\w+)$")))
+def snip(event):
+    snippet = snips.get(event.pattern_match[1])
+    if snippet is not None:
+        event.delete()
+        client.send_message(event.input_chat, snippet, reply_to=event.message.reply_to_msg_id)
+        raise events.StopPropagation
--- w.py
+++ w.py
@@ -11,7 +11,7 @@ logger = logging.getLogger("main")
 client = TelegramClient('the_game', secrets.api_id, secrets.api_hash, update_workers=1, spawn_read_thread=False)
 client.start()
 
-plugins = ("misc", "re")
+plugins = ("misc", "re", "snippets")
 
 for p in plugins:
     import_module(f"plugins.{p}")
